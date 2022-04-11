466ed5c5edb76fd164628e194b1b79a07f3daf55
Chinseok Lee <me@pyhub.kr>
2022-04-11 17:49:30 +0900

    bootstrap4에서 bootstrap5로 업그레이드

--- myproj/settings.py
+++ myproj/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4',
+    'bootstrap5',
     'accounts',
     'catube',
 ]
