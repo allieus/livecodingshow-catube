da969ea1a66fac9476dec3c1eed0093f685940b1
Chinseok Lee <me@pyhub.kr>
2022-04-11 21:38:55 +0900

    TIMEZONE을 지원하되 디폴트 timezone으로 Asia/Seoul 지정

---
+++ myproj/middleware.py
@@ -0,0 +1,19 @@
+import zoneinfo
+
+from django.utils import timezone
+
+
+# refs: https://docs.djangoproject.com/ko/4.0/topics/i18n/timezones/#selecting-the-current-time-zone
+
+
+class TimezoneMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        tzname = request.session.get('django_timezone', 'Asia/Seoul')
+        if tzname:
+            timezone.activate(zoneinfo.ZoneInfo(tzname))
+        else:
+            timezone.deactivate()
+        return self.get_response(request)
--- myproj/settings.py
+++ myproj/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myproj.middleware.TimezoneMiddleware',
 ]
 
 ROOT_URLCONF = 'myproj.urls'
