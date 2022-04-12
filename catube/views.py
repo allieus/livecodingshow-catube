064109c2ce2b128302ad976f6278f96ac27e42fd
Chinseok Lee <me@pyhub.kr>
2022-04-12 10:10:30 +0900

    비디오 생성/수정 폼 제목 추가

--- catube/views.py
+++ catube/views.py
@@ -31,6 +31,11 @@ class VideoCreateView(LoginRequiredMixin, CreateView):
     form_class = VideoForm
     template_name = "form.html"
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["form_title"] = "새 비디오 등록"
+        return context_data
+
     def form_valid(self, form):
         video = form.save(commit=False)
         video.author = self.request.user
@@ -60,6 +65,11 @@ class VideoUpdateView(UserPassesTestMixin, UpdateView):
 
     def test_func(self):
         return self.request.user == self.get_object().author
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["form_title"] = "비디오 수정"
+        return context_data
 
     def form_valid(self, form):
         response = super().form_valid(form)
